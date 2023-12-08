3ddeaa865d0e57881eb6fe61bb0302c47905b886
Kuba Orłowski <kuba.orlowski@protonmail.com>
2023-12-08 13:22:47 +0000

    Moved deck-related code to a separate module

---
+++ src/deck.rs
@@ -0,0 +1,91 @@
+pub enum CardColor {
+    Blue,
+    Red,
+    Yellow,
+    Green,
+}
+
+pub enum CardValue {
+    Adenine,
+    ThymineUracil,
+    Guanine,
+    Cytosine,
+}
+
+pub enum ActionType {
+    Stop,
+    Mutation,
+}
+
+impl ActionCard {
+    pub fn new(action: ActionType) -> ActionCard {
+        ActionCard { action }
+    }
+}
+
+pub struct BaseCard {
+    color: CardColor,
+    value: CardValue
+}
+
+impl BaseCard {
+    pub fn new(color: CardColor, value: CardValue) -> BaseCard {
+        BaseCard { color, value }
+    }
+}
+
+pub struct ActionCard {
+    action: ActionType,
+}
+
+pub enum DeckCard {
+    ActionCard(ActionCard),
+    BaseCard(BaseCard),
+}
+
+pub fn is_stop_card(card: &DeckCard) -> bool {
+    match card {
+        DeckCard::ActionCard(action_card) => match action_card.action {
+            ActionType::Stop => true,
+            ActionType::Mutation => false
+        },
+        DeckCard::BaseCard(_) => false
+    }
+}
+
+pub struct Deck {
+    cards: Vec<DeckCard>,
+    initial_size: usize,
+    stop_cards: u8,
+}
+
+impl Deck {
+    pub fn new() -> Deck {
+        Deck {
+            cards: vec![],
+            initial_size: 0,
+            stop_cards: 0,
+        }
+    }
+
+    pub fn from_vec(cards: Vec<DeckCard>) -> Deck {
+        let size = &cards.len();
+        let stop_cards = &cards
+            .iter()
+            .filter(|c| is_stop_card(c))
+            .count();
+        Deck {
+            cards: cards,
+            initial_size: *size,
+            stop_cards: *stop_cards as u8,
+        }
+    }
+
+    pub fn cards_left(&self) -> u8 {
+        self.cards.len() as u8
+    }
+
+    pub fn stop_cards_left(&self) -> u8 {
+        self.stop_cards
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,4 @@
-enum CardColor {
-    Blue,
-    Red,
-    Yellow,
-    Green,
-}
-
-enum CardValue {
-    Adenine,
-    ThymineUracil,
-    Guanine,
-    Cytosine,
-}
-
-enum ActionType {
-    Stop,
-    Mutation,
-}
-
-struct BaseCard {
-    color: CardColor,
-    value: CardValue
-}
-
-struct ActionCard {
-    action: ActionType,
-}
-
-enum DeckCard {
-    ActionCard(ActionCard),
-    BaseCard(BaseCard),
-}
-
-struct Deck {
-    cards: Vec<DeckCard>,
-    initial_size: usize,
-    stop_cards: u8,
-}
-
-fn is_stop_card(card: &DeckCard) -> bool {
-    match card {
-        DeckCard::ActionCard(action_card) => match action_card.action {
-            ActionType::Stop => true,
-            ActionType::Mutation => false
-        },
-        DeckCard::BaseCard(_) => false
-    }
-}
-
-impl Deck {
-    fn new() -> Deck {
-        Deck {
-            cards: vec![],
-            initial_size: 0,
-            stop_cards: 0,
-        }
-    }
-
-    fn from_vec(cards: Vec<DeckCard>) -> Deck {
-        let size = &cards.len();
-        let stop_cards = &cards
-            .iter()
-            .filter(|c| crate::is_stop_card(c))
-            .count();
-        Deck {
-            cards: cards,
-            initial_size: *size,
-            stop_cards: *stop_cards as u8,
-        }
-    }
-
-    fn cards_left(&self) -> usize {
-        self.cards.len()
-    }
-}
-
+mod deck;
 
 fn main() {
     println!("Hello, world!");
@@ -82,7 +7,7 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-    use crate::{ActionCard, ActionType, BaseCard, CardColor, CardValue, Deck, DeckCard};
+    use crate::deck::{ActionCard, ActionType, BaseCard, CardColor, CardValue, Deck, DeckCard};
 
     #[test]
     fn new_deck_is_empty() {
@@ -93,46 +18,28 @@ mod tests {
     #[test]
     fn recognise_stop_cards() {
         let cards: Vec<DeckCard> = vec![
-            DeckCard::ActionCard(ActionCard {
-                action: ActionType::Mutation,
-            }),
-            DeckCard::ActionCard(ActionCard {
-                action: ActionType::Stop,
-            }),
-            DeckCard::BaseCard(BaseCard {
-                color: CardColor::Blue, value: CardValue::Cytosine,
-            }),
+            DeckCard::ActionCard(ActionCard::new (ActionType::Mutation)),
+            DeckCard::ActionCard(ActionCard::new (ActionType::Stop)),
+            DeckCard::BaseCard(BaseCard::new (CardColor::Blue, CardValue::Cytosine)),
         ];
-        assert!(!crate::is_stop_card(&cards[0]));
-        assert!(crate::is_stop_card(&cards[1]));
-        assert!(!crate::is_stop_card(&cards[2]));
+        assert!(!crate::deck::is_stop_card(&cards[0]));
+        assert!(crate::deck::is_stop_card(&cards[1]));
+        assert!(!crate::deck::is_stop_card(&cards[2]));
     }
 
     #[test]
     fn deck_from_vec() {
         let cards: Vec<DeckCard> = vec![
-            DeckCard::ActionCard(ActionCard {
-                action: ActionType::Mutation,
-            }),
-            DeckCard::ActionCard(ActionCard {
-                action: ActionType::Stop,
-            }),
-            DeckCard::BaseCard(BaseCard {
-                color: CardColor::Blue, value: CardValue::Cytosine,
-            }),
-            DeckCard::BaseCard(BaseCard {
-                color: CardColor::Blue, value: CardValue::Adenine,
-            }),
-            DeckCard::BaseCard(BaseCard {
-                color: CardColor::Blue, value: CardValue::Adenine,
-            }),
-            DeckCard::BaseCard(BaseCard {
-                color: CardColor::Green, value: CardValue::Adenine,
-            }),
+            DeckCard::ActionCard(ActionCard::new (ActionType::Mutation)),
+            DeckCard::ActionCard(ActionCard::new (ActionType::Stop)),
+            DeckCard::BaseCard(BaseCard::new (CardColor::Blue, CardValue::Cytosine)),
+            DeckCard::BaseCard(BaseCard::new (CardColor::Blue, CardValue::Adenine)),
+            DeckCard::BaseCard(BaseCard::new (CardColor::Blue, CardValue::Adenine)),
+            DeckCard::BaseCard(BaseCard::new (CardColor::Green, CardValue::Guanine)),
         ];
         let deck = Deck::from_vec(cards);
 
         assert_eq!(deck.cards_left(), 6);
-        assert_eq!(deck.stop_cards, 1);
+        assert_eq!(deck.stop_cards_left(), 1);
     }
 }
