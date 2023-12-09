cfd04449cd8e7bf099a3000014a32561b7eb9a32
Kuba Orłowski <kuba.orlowski@protonmail.com>
2023-12-09 22:31:40 +0000

    Printing out card values

--- src/deck.rs
+++ src/deck.rs
@@ -1,8 +1,23 @@
+use std::fmt;
+use std::fmt::Formatter;
+
 pub enum CardColor {
     Blue,
     Red,
     Yellow,
     Green,
+}
+
+impl fmt::Display for CardColor {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        let color = match self {
+            CardColor::Blue => "Blue",
+            CardColor::Green => "Green",
+            CardColor::Yellow => "Yellow",
+            CardColor::Red => "Red",
+        };
+        write!(f, "{}", color)
+    }
 }
 
 pub enum CardValue {
@@ -12,9 +27,31 @@ pub enum CardValue {
     Cytosine,
 }
 
+impl fmt::Display for CardValue {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        let value = match self {
+            CardValue::Cytosine => "C",
+            CardValue::Adenine => "A",
+            CardValue::ThymineUracil => "T/U",
+            CardValue::Guanine => "G",
+        };
+        write!(f, "{}", value)
+    }
+}
+
 pub enum ActionType {
     Stop,
     Mutation,
+}
+
+impl fmt::Display for ActionType {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        let action_type = match self {
+            ActionType::Stop => "Stop",
+            ActionType::Mutation => "Mutation",
+        };
+        write!(f, "{}", action_type)
+    }
 }
 
 impl ActionCard {
@@ -41,6 +78,16 @@ pub struct ActionCard {
 pub enum DeckCard {
     ActionCard(ActionCard),
     BaseCard(BaseCard),
+}
+
+impl fmt::Display for DeckCard {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        let t = match self {
+            DeckCard::ActionCard(c) => format!("Action {}", c.action),
+            DeckCard::BaseCard(c) => format!("Base {} {}", c.color, c.value),
+        };
+        write!(f, "{}", t)
+    }
 }
 
 pub fn is_stop_card(card: &DeckCard) -> bool {
@@ -92,13 +139,20 @@ impl Deck {
     pub fn draw(&mut self, how_many: u8) -> Vec<DeckCard> {
         let mut new_cards= vec![];
         let how_many = how_many as usize;
+        let mut stop_cards_drawn: u8 = 0;
         while new_cards.len() < how_many {
             let card = self.cards.pop();
             match card {
-                Some(card) => new_cards.push(card),
-                None => return new_cards,
+                Some(c) => {
+                    if is_stop_card(&c) {
+                        stop_cards_drawn += 1;
+                    }
+                    new_cards.push(c);
+                },
+                None => break
             }
         }
+        self.stop_cards -= stop_cards_drawn;
         new_cards
     }
 
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,39 @@
+use crate::deck::{ActionCard, ActionType, BaseCard, CardColor, CardValue, DeckCard};
+
 mod deck;
 
 fn main() {
-    println!("Hello, world!");
+    let cards: Vec<DeckCard> = vec![
+        DeckCard::ActionCard(ActionCard::new (ActionType::Mutation)),
+        DeckCard::BaseCard(BaseCard::new (CardColor::Blue, CardValue::Cytosine)),
+        DeckCard::BaseCard(BaseCard::new (CardColor::Blue, CardValue::Cytosine)),
+        DeckCard::BaseCard(BaseCard::new (CardColor::Blue, CardValue::ThymineUracil)),
+        DeckCard::BaseCard(BaseCard::new (CardColor::Yellow, CardValue::Adenine)),
+        DeckCard::BaseCard(BaseCard::new (CardColor::Red, CardValue::Adenine)),
+        DeckCard::BaseCard(BaseCard::new (CardColor::Green, CardValue::Guanine)),
+        DeckCard::ActionCard(ActionCard::new (ActionType::Stop)),
+    ];
+    for card in cards.iter() {
+        println!("{}", card);
+    }
 }
 
 
 #[cfg(test)]
 mod tests {
-    use crate::deck::{ActionCard, ActionType, BaseCard, CardColor, CardValue, Deck, DeckCard};
+    use crate::deck::{ActionCard, ActionType, BaseCard, CardColor, CardValue, Deck, DeckCard, is_stop_card};
+
+    fn create_example_deck() -> Deck {
+        let cards: Vec<DeckCard> = vec![
+            DeckCard::ActionCard(ActionCard::new (ActionType::Mutation)),
+            DeckCard::BaseCard(BaseCard::new (CardColor::Blue, CardValue::Cytosine)),
+            DeckCard::BaseCard(BaseCard::new (CardColor::Blue, CardValue::Adenine)),
+            DeckCard::BaseCard(BaseCard::new (CardColor::Blue, CardValue::Adenine)),
+            DeckCard::BaseCard(BaseCard::new (CardColor::Green, CardValue::Guanine)),
+            DeckCard::ActionCard(ActionCard::new (ActionType::Stop)),
+        ];
+        Deck::from_vec(cards)
+    }
 
     #[test]
     fn new_deck_is_empty() {
@@ -18,9 +44,9 @@ mod tests {
     #[test]
     fn recognise_stop_cards() {
         let cards: Vec<DeckCard> = vec![
-            DeckCard::ActionCard(ActionCard::new (ActionType::Mutation)),
-            DeckCard::ActionCard(ActionCard::new (ActionType::Stop)),
             DeckCard::BaseCard(BaseCard::new (CardColor::Blue, CardValue::Cytosine)),
+            DeckCard::ActionCard(ActionCard::new (ActionType::Stop)),
+            DeckCard::ActionCard(ActionCard::new (ActionType::Mutation)),
         ];
         assert!(!crate::deck::is_stop_card(&cards[0]));
         assert!(crate::deck::is_stop_card(&cards[1]));
@@ -29,15 +55,7 @@ mod tests {
 
     #[test]
     fn deck_from_vec() {
-        let cards: Vec<DeckCard> = vec![
-            DeckCard::ActionCard(ActionCard::new (ActionType::Mutation)),
-            DeckCard::ActionCard(ActionCard::new (ActionType::Stop)),
-            DeckCard::BaseCard(BaseCard::new (CardColor::Blue, CardValue::Cytosine)),
-            DeckCard::BaseCard(BaseCard::new (CardColor::Blue, CardValue::Adenine)),
-            DeckCard::BaseCard(BaseCard::new (CardColor::Blue, CardValue::Adenine)),
-            DeckCard::BaseCard(BaseCard::new (CardColor::Green, CardValue::Guanine)),
-        ];
-        let deck = Deck::from_vec(cards);
+        let deck = create_example_deck();
 
         assert_eq!(deck.cards_left(), 6);
         assert_eq!(deck.stop_cards_left(), 1);
